f7bfd37e4846d46e6bc3e18f39ec525e04c71589
Darik <darikmesele10@gmail.com>
2022-06-27 17:57:18 +0300

    Add project files.

---
+++ OddEven.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class OddEven
+{
+    static void Main(string[] args)
+    {
+        int choice;
+        do
+        {
+            int num;
+            Console.WriteLine("Odd or Even");
+            Console.WriteLine("Please enter a number:");
+            num = Convert.ToInt32(Console.ReadLine());
+            {
+                if (num % 2 == 0)
+                {
+                    Console.WriteLine("Even");
+                }
+                else
+                {
+                    Console.WriteLine("Odd");
+                }
+                Console.WriteLine("1.Exit");
+
+                choice = Convert.ToInt32(Console.ReadLine());
+            }
+        } while (choice != 1);
+    }
+}
